04b80c981b2949b4c5cc530a6894b50de2a92abb
Gabryella-Silva <gabryellaspereira7@gmail.com>
2024-10-20 20:49:56 -0300

    criação e finalização dos botões da home

---
+++ src/app/assets/images/index.tsx
@@ -0,0 +1,13 @@
+import Clipboard from './Clipboard.png'
+import Vaccine from './Vaccine.png'
+import Veterinarian from './Veterinarian.png'
+import Veterinary from './Veterinary.png'
+
+export const Images = {
+    Clipboard,
+    Vaccine,
+    Veterinarian,
+    Veterinary
+
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import { Images } from "./assets/images";
+import { Button } from "./components/button";
 import { MainPage } from "./components/MainPage";
+
 
 export default function Home() {
   return (
     <>
+    <header className="flex gap-8 items-center justify-center mt-8">
+      <Button Image  ={Images.Veterinarian} h1="Internato" p="Registre internamentos"/>
+      <Button Image={Images.Veterinary} h1="Consultas" p="Crie consultas e gerencie-as"/>
+      <Button Image={Images.Vaccine} h1="Recursos" p="Exporte planilhas e gerencie recursos"/>
+      <Button Image={Images.Clipboard} h1="Exames" p="Crie exames e precrições"/>
+    </header>
+    
       <MainPage />
     </>
   );
