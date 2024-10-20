ea8b62b6b5a170c68c6ac917cb030c85160be986
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2024-10-19 21:51:55 -0300

    Finalizando a feature componente-main

---
+++ src/app/components/AsideMain.tsx
@@ -0,0 +1,16 @@
+export function AsideMain() {
+  return (
+    <>
+      <aside className="bg-mascots-primary-600 p-4 font-sans rounded-xl">
+        <nav className="flex flex-row gap-2 justify-center">
+          <div className="w-1/2 flex justify-between">
+            <a href="#">link</a>
+            <a href="#">link</a>
+            <a href="#">link</a>
+            <a href="#">link</a>
+          </div>
+        </nav>
+      </aside>
+    </>
+  );
+}
