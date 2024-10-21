a51a489c51e6c6641ba2943cde41f13787a70cd4
Gabryella-Silva <gabryellaspereira7@gmail.com>
2024-10-20 21:20:54 -0300

    criação dos botões da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
     <header className="flex gap-8 items-center justify-center mt-8">
-      <Button Image  ={Images.Veterinarian} h1="Internato " p="Registre internamentos"/>
+      <Button Image  ={Images.Veterinarian} h1="Internato" p="Registre internamentos"/>
       <Button Image={Images.Veterinary} h1="Consultas" p="Crie consultas e gerencie-as"/>
       <Button Image={Images.Vaccine} h1="Recursos" p="Exporte planilhas e gerencie recursos"/>
       <Button Image={Images.Clipboard} h1="Exames" p="Crie exames e precrições"/>
