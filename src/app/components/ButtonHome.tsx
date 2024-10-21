b1ba8b5561370f31c922579a4275313187ffdd56
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2024-10-21 14:15:22 -0300

    refactor: buttons home name e tag button

---
+++ src/app/components/ButtonHome.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { StaticImageData } from "next/image";
+
+type ButtonProps = {
+  Image: string | StaticImageData;
+  h1: string;
+  p: string;
+};
+export function ButtonHome(props: ButtonProps) {
+  return (
+    <button className="flex items-center gap-5 p-5 bg-mascots-primary-600 max-w-64 border rounded-xl transition-all duration-300 hover:bg-mascots-primary-700 cursor-pointer font-sans shadow-custom text-white">
+      <div className="p-4 bg-mascots-primary-50 border rounded-full ">
+        <Image src={props.Image} alt={props.h1} width={24} height={24} />
+      </div>
+
+      <div className="text-left">
+        <h1>
+          {" "}
+          <b> {props.h1} </b>
+        </h1>
+        <p> {props.p} </p>
+      </div>
+    </button>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
 import { Images } from "./assets/images";
-import { Button } from "./components/button";
+import { ButtonHome } from "./components/ButtonHome";
 import { MainPage } from "./components/MainPage";
-
 
 export default function Home() {
   return (
     <>
-    <header className="flex gap-8 items-center justify-center mt-8">
-      <Button Image  ={Images.Veterinarian} h1="Internato" p="Registre internamentos"/>
-      <Button Image={Images.Veterinary} h1="Consultas" p="Crie consultas e gerencie-as"/>
-      <Button Image={Images.Vaccine} h1="Recursos" p="Exporte planilhas e gerencie recursos"/>
-      <Button Image={Images.Clipboard} h1="Exames" p="Crie exames e precrições"/>
-    </header>
-    
+      <header className="flex gap-8 items-center justify-center mt-8">
+        <ButtonHome
+          Image={Images.Veterinarian}
+          h1="Internato"
+          p="Registre internamentos"
+        />
+        <ButtonHome
+          Image={Images.Veterinary}
+          h1="Consultas"
+          p="Crie consultas e gerencie-as"
+        />
+        <ButtonHome
+          Image={Images.Vaccine}
+          h1="Recursos"
+          p="Exporte planilhas e gerencie recursos"
+        />
+        <ButtonHome
+          Image={Images.Clipboard}
+          h1="Exames"
+          p="Crie exames e precrições"
+        />
+      </header>
+
       <MainPage />
     </>
   );
