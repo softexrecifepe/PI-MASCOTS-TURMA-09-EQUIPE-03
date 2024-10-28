0ed75f33e1d29da3527c4ab9925385f5f586778a
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2024-10-28 14:35:18 -0300

    refactor: mudancas dimensoes tela sigin

--- src/app/components/LoginPageButton.tsx
+++ src/app/components/LoginPageButton.tsx
@@ -4,7 +4,7 @@ interface loginPageButtonProps {
 
 export function LoginPageButton({ text }: loginPageButtonProps) {
   return (
-    <button className="py-2 bg-mascots-primary-600 rounded-xl text-white  hover:bg-mascots-primary-800 active:bg-mascots-primary-500 transition-all">
+    <button className="py-2 bg-mascots-primary-600 rounded-xl text-white mt-4 hover:bg-mascots-primary-800 active:bg-mascots-primary-500 transition-all">
       {text}
     </button>
   );
---
+++ src/app/login/LogoForms.tsx
@@ -0,0 +1,9 @@
+import logo from "../assets/images/Logo.png";
+
+export function LogoForms() {
+  return (
+    <div className="w-2/3">
+      <img src={logo.src} />
+    </div>
+  );
+}
